cf7d5d9ef218c6c4c32861f9f654fd20ef27fb07
chito <marco.96.12@gmail.com>
2023-01-23 18:50:28 +0100

    first test openAI

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 class Env:
-  __init__():
-    key: "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"
+  def __init__(self):
+    self.key = "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"
+    pass
+  def get_gey(self):
+    return self.key
+
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-import os
+
+from pyexpat import model
 import openai
-import Env from env
+from env import Env
 
 env = Env()
 
-openai.api_key = os.getenv(env.key)
+openai.api_key = env.get_gey()
 
+'''
 response = openai.Completion.create(
   model="text-davinci-001",
-  prompt="Write a tagline for an ice cream shop.",
-  temperature=0.4,
-  max_tokens=64,
+  prompt="devo studaire per esame di Sistemi Informativi (per il settore dell’informazione), questo è un Esempio di domande d’esame,- discutere cosa si intende per sistema di gestione di workflow. prova a rispondere alla domanda",
+  temperature=0.7,
+  max_tokens=300,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
 )
+
+print(response)
+'''
+
+# request in open ai for a 1920 immage of a cat 
+response = openai.Image.create(
+  prompt="a white siamese cat, in futuristic style, with a blue background",
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
+
+print(image_url)
