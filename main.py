13b92aec396c04a19f613dbb1ac67bb134b1aa9d
Marco Selva <39164743+marcoselva@users.noreply.github.com>
2023-01-23 18:28:27 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import openai
+import Env from env
+
+openai.api_key = os.getenv(Env.key)
+
+response = openai.Completion.create(
+  model="text-davinci-001",
+  prompt="Write a tagline for an ice cream shop.",
+  temperature=0.4,
+  max_tokens=64,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
