26e766b8f640eb9ce6f82392c03938d355c69051
kito129 <marco.96.12@gmail.com>
2023-04-07 22:13:13 +0200

    clean repo

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# kitoAi
+# kito.ai
 
-#openAi
-
-Key: 
-  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
--- env.py
+++
@@ -1,7 +0,0 @@
-class Env:
-  def __init__(self):
-    self.key = "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"
-    pass
-  def get_gey(self):
-    return self.key
-
