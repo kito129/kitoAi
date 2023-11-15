b87f64ff22464a282a8f2f370f2105051fae7b73
kito129 <marco.96.12@gmail.com>
2023-11-16 00:17:00 +0100

    remove ssr

--- developing/frontend/kitoAi/src/app/features/home/services/home.service.ts
+++ developing/frontend/kitoAi/src/app/features/home/services/home.service.ts
@@ -8,13 +8,15 @@ import {Home} from "../model/home";
 })
 export class HomeService {
 
+	prefix = 'http://localhost:8090'
+
   constructor(private http: HttpClient) { }
 
 	getHomeElements(): Observable<Array<Home>> {
-		return this.http.get<Array<Home>>(`/api/collections/home/records}`);
+		return this.http.get<Array<Home>>(`${this.prefix}/api/collections/home/records/`)
 	}
 
 	getHomeElement(id: string): Observable<Home> {
-		return this.http.get<Home>(`/api/collections/home/records/${id}`);
+		return this.http.get<Home>(`${this.prefix}/api/collections/home/records/${id}`)
 	}
 }
