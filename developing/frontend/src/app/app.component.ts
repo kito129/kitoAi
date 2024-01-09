16e68004f7f5031ff5456178da4a36173091f029
kito129 <marco.96.12@gmail.com>
2024-01-09 23:30:36 +0100

    fix app component CSS and layout

--- developing/frontend/src/app/app.component.ts
+++ developing/frontend/src/app/app.component.ts
@@ -24,61 +24,71 @@ import {MENU, MenuItem} from "./shared/models/menu";
 		MatButtonModule
 	],
   template: `
-      <div class="wrapper">
-          <!-- Header -->
-          <mat-toolbar color="primary" class="toolbar">
-                  <button mat-icon-button (click)="sidenav.toggle()">
-                      <mat-icon>menu</mat-icon>
-                  </button>
-                  <h2>Kito.ai</h2>
-                  <mat-icon>donut_small</mat-icon>
-                  <button mat-icon-button
-                          *ngIf="(authServices.isLogged$() | async)"
-                          (click)="authServices.logout()">
-                      <mat-icon>logout</mat-icon>
-                  </button>
-          </mat-toolbar>
-          <!--- Side Navigation -->
-          <mat-sidenav-container>
-              <mat-sidenav #sidenav mode="over">
-                  <mat-nav-list>
-                      <a mat-list-item routerLink="/public">Public</a>
-                      <mat-divider></mat-divider>
-                      <ng-container *ngIf="!(authServices.isLogged$() | async), else logged">
-                          <a mat-list-item
-                             routerLink="/auth/login">Login</a>
-                      </ng-container>
-                      <ng-template #logged>
-                          <a *ngFor="let item of menuItems" mat-list-item [routerLink]="item.link">
-                              {{item.label}}
-                          </a>
+          <div class="wrapper">
+              <!-- Header -->
+              <mat-toolbar color="primary" class="fixed-top z-2">
+                  <div class="container-fluid ">
+                      <div class="row">
+                          <div class="col  d-flex justify-content-start">
+                              <button mat-icon-button (click)="sidenav.toggle()">
+                                  <mat-icon>menu</mat-icon>
+                              </button>
+                          </div>
+                          <div class="col d-flex justify-content-center">
+                              <h2>Kito.ai  <mat-icon>donut_small</mat-icon> </h2>
+                          </div>
+                          <div class="col d-flex justify-content-end">
+                              <button mat-icon-button *ngIf="(authServices.isLogged$() | async)" (click)="authServices.logout()">
+                                  <mat-icon>logout</mat-icon>
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </mat-toolbar>
+
+              <!-- Side Navigation -->
+              <mat-sidenav-container>
+                  <mat-sidenav #sidenav mode="over">
+                      <mat-nav-list>
+                          <a mat-list-item routerLink="/public">Public</a>
                           <mat-divider></mat-divider>
-                          <a mat-list-item (click)="authServices.logout()">LogOut</a>
-                      </ng-template>
-                  </mat-nav-list>
-              </mat-sidenav>
-              <!-- Main Content Area -->
-              <mat-sidenav-content class="container-fluid" style="height: 1280px">
-                <router-outlet class="router"></router-outlet>
-              </mat-sidenav-content>
-          </mat-sidenav-container>
-            <!-- Footer -->
-            <mat-toolbar class="fixed-bottom text-white-50 flex-end" style="height: 30px">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col">
-                            <p>Copyright © {{date.getFullYear()}}kito.ai. All rights reserved</p>
-                        </div>
-                        <div class="col">
-                            <p>v0.0.2 - Build #: 2</p>
-                        </div>
-                        <div class="col">
-                            <p>Build: 01-08-2024</p>
-                        </div>
-                    </div>
-                </div>
-            </mat-toolbar>
-      </div>
+                          <ng-container *ngIf="!(authServices.isLogged$() | async), else logged">
+                              <a mat-list-item routerLink="/auth/login">Login</a>
+                          </ng-container>
+                          <ng-template #logged>
+                              <a *ngFor="let item of menuItems" mat-list-item [routerLink]="item.link">
+                                  {{item.label}}
+                              </a>
+                              <mat-divider></mat-divider>
+                              <a mat-list-item (click)="authServices.logout()">LogOut</a>
+                          </ng-template>
+                      </mat-nav-list>
+                  </mat-sidenav>
+
+                  <!-- Main Content Area -->
+                  <mat-sidenav-content class="container-fluid" style="height: 100vh; padding-top: 70px">
+                      <router-outlet class="router" ></router-outlet>
+                  </mat-sidenav-content>
+              </mat-sidenav-container>
+
+              <!-- Footer -->
+              <mat-toolbar class="fixed-bottom small text-white-50 text-center align-bottom" style="height: 30px">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col">
+                              <p>Copyright © {{date.getFullYear()}} kito.ai. All rights reserved</p>
+                          </div>
+                          <div class="col">
+                              <p>v0.0.2 - Build #: 2</p>
+                          </div>
+                          <div class="col">
+                              <p>Build: 01-08-2024</p>
+                          </div>
+                      </div>
+                  </div>
+              </mat-toolbar>
+          </div>
+
   `,  styleUrl: `./app.component.css`
 })
 
